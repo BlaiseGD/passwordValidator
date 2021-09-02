6ec8f479970d9d56529f514889888a244570a3b6
BlaiseGD <58518531+BlaiseGD@users.noreply.github.com>
2021-09-01 20:51:23 -0500

    Added colors and password generation function
    Used Ansi escape codes to highlight more important text and to make program look better.
    The password generating function may make an insecure password by chance.  I need to fix that with maybe a custom algorithm to ensure the password is valid. I could also give a password generator for valid or invalid passwords depending on the users choice.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
 int length;
-string generated;
 string password;
 
+
 /*this functon finds if the password is valid by going through a range of ascii numbers and returning a bool based on if the conditions are met*/
-void validator(){
+bool validator(){
   bool specialChar = false; //decides if there is or isnt special characters
   bool numbers = false; //decides if there are numbers or not
   bool upper = false; //decides if there are uppercase characters
@@ -30,7 +31,7 @@ if(password.length() > 999){
   }
   if(numbers != true){
     cout << "The password is not valid! It doesn't contain any numbers!" << endl << endl;
-    goto invalid; //this goto statement is at end of function to shorten runtime
+    goto invalid;
   }
   //this for loop searches for special characters
   for(char i = 33; i != '~'; i++){
@@ -80,34 +81,41 @@ if(password.length() > 999){
 
 if(specialChar && numbers && upper && lower == true && password.length() > 12){
   cout << endl <<"The password is valid and safe!" << endl << endl;
+  return true;
 }
 invalid:
-cout << "";
+return false;
 }
 
-//W.I.P
-string generatedPswd(){
-  cout << "WORK IN PROGRESS";
-return generated;
+void generatedPswd(){
+  char generated[length];
+  random_device rd; // obtain a random number from hardware
+  mt19937 gen(rd()); // seed the generator
+  uniform_int_distribution<> distr(33, 126); // define the range
+  for(int i = 0; i < length; i++){
+    generated[i] = distr(gen);
+  }
+  cout << endl << "Generated password is:";
+  cout << endl << endl << "\u001b[33m" <<generated << "\e[0;37m" << endl << endl;
 }
 
 int main() {
-  char reset;
+  string reset;
   int menuchoice;
   errorcatch:
   cout << "This is a password generator and validator! " << endl;
-  cout << "Y signifies yes and N signifies no" << endl;
+  cout << "\e[1;31m 'y' signifies yes and 'n' signifies no\e[0;37m" << endl;
   cout << "Type in the number that corresponds to your choice" << endl << endl;
-  cout << "1. Password Generator" << endl;
-  cout << "2. Password Validator" << endl;
-  cout << "3. Password Strength Info" << endl;
+  cout << "\u001b[34m1. Password Generator\e[0;37m" << endl;
+  cout << "\u001b[33m2. Password Validator\e[0;37m" << endl;
+  cout << "\u001b[32m3. Password Strength Info\e[0;37m" << endl;
   cin >> menuchoice;
   system("clear");
   switch(menuchoice){
     case 1:
       cout << "What length do you want the password to be?" <<endl;
       cin >> length;
-      if(length < 999){
+      if(length > 999){
         cout << " Error: Password length exceeds maxiumum" << endl << endl;
         fflush(stdin);
         goto errorcatch;
@@ -119,7 +127,7 @@ int main() {
     validator();
     break;
     case 3:
-    cout << "A good password constitutes at least one special character, one number, uppercase, and lowercase letters." << endl << "I usually recommend a password length of at least 12." <<endl << "The length and number of the aforementioned character types depends on your usecase." << endl <<  "If you are trying to hide from the feds than I think you better get off my github and do something much more technical to escape them. Good luck with that!" << endl << "If you're just an average employee or businessman than this program should work well enough for you." << endl << endl << "\e[1;31m WARNING! Computers can NEVER be truly random, so use the program at your own risk. \e[0;37m" <<endl << endl;
+    cout << "A good password constitutes at least one special character, one number, uppercase, and lowercase letters." << endl << "I usually recommend a password length of at least 12." <<endl << "The length and number of the aforementioned character types depends on your usecase." << endl <<  "If you are trying to hide from the feds than I think you better get off my github and do something much more technical to escape them. Good luck with that!" << endl << "If you're just an average employee or businessman than this program should work well enough for you." << endl << endl << "\e[1;31m WARNING! Computers can NEVER be truly random, so use the program at your own risk." << endl << " The generated password may not be valid.\e[0;37m" << endl << endl;
     break;
     default:
       cout << "An error occured";
@@ -128,13 +136,13 @@ int main() {
   }
   cout << "Do you want to create or validate another password?" << endl;
   cin >> reset;
-  if(reset == 'n' || 'N'){
+  if(reset == "n"){
     system("clear");
     cout << "Thanks for using my validator, and have a nice day :)";
     return 0;
   }
     fflush(stdin);
     system("clear");
-    goto errorcatch; //resets the program if they choose anything other than n or N
+    goto errorcatch; //resets the program if they choose anything other than n
   return 0;
 }
